c97c85e1c024c917fd769c0af895733e23112c2f
mattmellow <matthewgay123@gmail.com>
2025-09-25 21:53:27 +0800

    added dashing for mario

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -24,6 +24,13 @@ public class PlayerMovement : MonoBehaviour
 
     private bool isDead;
 
+    // Dash Variables
+    public float dashSpeed = 50f;
+    public float dashDuration = 0.2f;
+    private bool isDashing = false;
+    private float dashTime = 0f;
+    private Vector2 dashDirection;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,12 +38,13 @@ public class PlayerMovement : MonoBehaviour
         Application.targetFrameRate = 30;
         marioBody = GetComponent<Rigidbody2D>();
         marioSprite = GetComponent<SpriteRenderer>();
-
     }
 
     void Update()
     {
         float moveHorizontal = Input.GetAxisRaw("Horizontal");
+
+        // Flip character sprite when moving left or right
         if (moveHorizontal < 0 && faceRightState)
         {
             faceRightState = false;
@@ -47,6 +55,78 @@ public class PlayerMovement : MonoBehaviour
         {
             faceRightState = true;
             marioSprite.flipX = false;
+        }
+
+        // Space key action for dashing
+        if (Input.GetKeyDown(KeyCode.Space) && !isDashing)
+        {
+            marioBody.linearVelocity = new Vector2(0, 0); // Stop vertical velocity
+            isDashing = true;
+            dashTime = 0f;
+
+            // Set dash direction based on current movement direction
+            dashDirection = new Vector2(marioSprite.flipX ? -1 : 1, 0); // Dash left or right
+
+            // Start dash
+            marioBody.linearVelocity = new Vector2(dashDirection.x * dashSpeed, marioBody.linearVelocity.y);
+        }
+
+        // W key for jump
+        if (Input.GetKeyDown(KeyCode.W) && onGroundState)
+        {
+            marioBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            onGroundState = false;
+            isJumping = true;
+        }
+
+        // Hold jump: apply smaller force while rising
+        if (Input.GetKey(KeyCode.W) && isJumping)
+        {
+            if (marioBody.linearVelocity.y > 0 && marioBody.linearVelocity.y < maxJumpVelocity)
+            {
+                marioBody.AddForce(Vector2.up * holdForce, ForceMode2D.Force);
+            }
+        }
+
+        // Short hop: if released early, cut upward velocity
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            if (marioBody.linearVelocity.y > 0)
+            {
+                marioBody.linearVelocity = new Vector2(marioBody.linearVelocity.x, marioBody.linearVelocity.y * 0.25f);
+            }
+            isJumping = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isDashing)
+        {
+            dashTime += Time.fixedDeltaTime;
+            if (dashTime >= dashDuration)
+            {
+                // End dash after duration
+                isDashing = false;
+                marioBody.linearVelocity = new Vector2(0, marioBody.linearVelocity.y); // Stop dash movement
+            }
+        }
+
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+
+        // Move player horizontally (when not dashing)
+        if (!isDashing && Mathf.Abs(moveHorizontal) > 0)
+        {
+            Vector2 movement = new Vector2(moveHorizontal, 0);
+            // check if it doesn't go beyond maxSpeed
+            if (marioBody.linearVelocity.magnitude < maxSpeed)
+                marioBody.AddForce(movement * speed);
+        }
+
+        // Stop player when keys are released
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            marioBody.linearVelocity = new Vector2(0, marioBody.linearVelocity.y);
         }
     }
 
@@ -63,57 +143,6 @@ public class PlayerMovement : MonoBehaviour
         if (col.gameObject.CompareTag("Ground")) onGroundState = true;
     }
 
-    // FixedUpdate may be called once per frame. See documentation for details.
-    void FixedUpdate()
-    {
-        float moveHorizontal = Input.GetAxisRaw("Horizontal");
-
-        if (Mathf.Abs(moveHorizontal) > 0)
-        {
-            Vector2 movement = new Vector2(moveHorizontal, 0);
-            // check if it doesn't go beyond maxSpeed
-            if (marioBody.linearVelocity.magnitude < maxSpeed)
-                marioBody.AddForce(movement * speed);
-        }
-
-        // stop
-        if (Input.GetKeyUp("a") || Input.GetKeyUp("d"))
-        {
-            // stop
-            marioBody.linearVelocity = Vector2.zero;
-        }
-
-
-        // Making the jump more like the actual mario game
-        // Start jump
-        if (Input.GetKeyDown("space") && onGroundState)
-        {
-            marioBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            onGroundState = false;
-            isJumping = true;
-        }
-
-        // Hold jump: apply smaller force while rising
-        if (Input.GetKey("space") && isJumping)
-        {
-            if (marioBody.linearVelocity.y > 0 && marioBody.linearVelocity.y < maxJumpVelocity)
-            {
-                marioBody.AddForce(Vector2.up * holdForce, ForceMode2D.Force);
-            }
-        }
-
-        // Short hop: if released early, cut upward velocity
-        if (Input.GetKeyUp("space"))
-        {
-            if (marioBody.linearVelocity.y > 0)
-            {
-                marioBody.linearVelocity = new Vector2(marioBody.linearVelocity.x, marioBody.linearVelocity.y * 0.25f);
-            }
-            isJumping = false;
-        }
-    }
-
-
     public void RestartButtonCallback(int input)
     {
         Debug.Log("Restart!");
@@ -127,6 +156,4 @@ public class PlayerMovement : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
-
-
 }
